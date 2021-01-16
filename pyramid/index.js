d42e0a6b5bf23b217920d9bb3493dd6981456043
Varun Raj <varunraj82@gmail.com>
2021-01-16 13:16:11 -0500

    pyramid in progress

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,29 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// 1,3,5,7,9,11
+// 1,2,3,4,5,6
+// n+(n-1)
+
+
+function pyramid(n) {
+
+    const baseLen = n+(n-1)
+
+    for (let i=1; i <=n; i ++){
+        
+        let str="";
+
+        for (let k=1; k <=baseLen ; k ++){
+            str+="#"
+        }
+
+        console.log(str);
+    }
+
+}
+
+
+pyramid(3)
 
 module.exports = pyramid;
