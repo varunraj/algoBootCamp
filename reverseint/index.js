3797cdc1a5949780d75406b83f014b8f86342ab6
Varun Raj <varunraj82@gmail.com>
2021-01-16 09:05:31 -0500

    added reverse int solution

--- reverseint/index.js
+++ reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    
+    const arr = n.toString().split('');
+    
+    if (arr[0] === "-" ){
+        arr.shift()
+        return parseInt(arr.reverse().join(''),10) * -1 
+    } else 
+        return parseInt(arr.reverse().join(''),10) 
+    
+}
 
 module.exports = reverseInt;
